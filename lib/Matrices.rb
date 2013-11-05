18dc8eb07d7e4210f92f9a3c8acb93573e3402f3
Nestor Albelo <alu0100598322@ull.edu.es>
2013-11-05 13:01:29 +0000

    Añadidos metodos

--- lib/Matrices.rb
+++ lib/Matrices.rb
@@ -1,8 +1,8 @@
 class Matrices
-	attr_reader :ancho, :alto, :matriz
-	def initialized(*elements, ancho)
-		raise "Los parametros deben ser de tipo Array, int, int y del tamaño correcto"
-				unless elements.instance_of?Array || (ancho*ancho) == elements.size 
+	attr_reader :ancho, :matriz
+	def initialize(*elements, ancho)
+		#raise "Los parametros deben ser de tipo Array, int, int y del tamaño correcto"
+		#		unless elements.instance_of?Array || (ancho*ancho) != elements.size 
 					@matriz[ancho][ancho]=0
 					for i in 0..ancho
 						for j in 0..ancho
@@ -10,12 +10,36 @@ class Matrices
 						end
 					end
 					@ancho=ancho
-				end
-		#end
+		#		end
+
 	end
-	def multiplicacion (other)
-		raise "No se puede multiplicar el elemento"
-			unless other.instance_of?Matrices || alto!=other.ancho
+	def +(other)
+		#raise "No se puede multiplicar el elemento"
+		#	unless other.instance_of?Matrices || ancho!=other.ancho
+				Resultado[ancho*ancho]
+				for i in 0..ancho
+					for j in 0..ancho
+						Resultado[i][j]=matriz[i][j]+other.matriz[i][j]
+					end
+				end
+				Resultado
+		#	end
+	end
+	def -(other)
+		#raise "No se puede multiplicar el elemento"
+		#	unless other.instance_of?Matrices || ancho!=other.ancho
+				Resultado[ancho*ancho]
+				for i in 0..ancho
+					for j in 0..ancho
+						Resultado[i][j]=matriz[i][j]-other.matriz[i][j]
+					end
+				end
+				Resultado
+		#	end
+	end
+	def *(other)
+		#raise "No se puede multiplicar el elemento"
+		#	unless other.instance_of?Matrices || ancho!=other.ancho
 				Resultado[ancho*ancho]
 				for i in 0..ancho
        				 for j in 0..ancho
@@ -23,10 +47,17 @@ class Matrices
                         		Resultado[i][j]+=matriz[i][k]*other.matriz[k][j]
                 			end		
         			end
-				end				
-			end
-		#end
+				end
+				Resultado				
+		#	end
 	end
-	
-
+	def Opuesto
+		Resultado[ancho*ancho]
+		for i in 0..ancho
+       		for j in 0..ancho
+       			Resultado[i][j]=-matriz[i][j]
+       		end
+       	end
+       	Resultado
+	end
 end
--- spec/Matrices_spec.rb
+++ spec/Matrices_spec.rb
@@ -2,6 +2,23 @@ require "Matrices.rb"
 
 describe Matrices do  
 	before :each do
-		#@m1=Matrices.new(Array=[1,2,3,4,5,6,7,8,9],3)
+		@Arr = [10,10,10,10,10,10,10,10,10]
+		@m1=Matrices.new([1,2,3,4,5,6,7,8,9],3)
+		@m2=Matrices.new([9,8,7,6,5,4,3,2,1],3)
 	end
+	describe "Operaciones de Matrices" do
+                it "Sumar" do
+                        (@m1+@m2).should eq(Matrices.new(@Arr,3))
+                end
+                it "Restar" do
+                        (@m1-@m2).should eq(Matrices.new([-8,-6,-4,-2,0,2,4,6,8],3))
+                end
+                it "Multiplicar" do
+						(@m1*@m2).should eq(Matrices.new([30,24,18,84,69,54,138,114,90],3))
+                end
+                it "Opuesto" do
+                		@m1.Opuesto.should eq(Matrices.new([-1,-2,-3,-4,-5,-6,-7,-8,-9],3))
+                end
+                
+    end
 end
