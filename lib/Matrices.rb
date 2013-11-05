d5ce79dd4ecc1bc1c60f9e44d2a6f41362108e81
Nestor Albelo <alu0100598322@ull.edu.es>
2013-11-05 12:18:48 +0000

    Clase creada

--- lib/Matrices.rb
+++ lib/Matrices.rb
@@ -1,3 +1,32 @@
 class Matrices
+	attr_reader :ancho, :alto, :matriz
+	def initialized(*elements, ancho)
+		raise "Los parametros deben ser de tipo Array, int, int y del tamaño correcto"
+				unless elements.instance_of?Array || (ancho*ancho) == elements.size 
+					@matriz[ancho][ancho]=0
+					for i in 0..ancho
+						for j in 0..ancho
+							matriz[i][j]=elements[i][j]
+						end
+					end
+					@ancho=ancho
+				end
+		#end
+	end
+	def multiplicacion (other)
+		raise "No se puede multiplicar el elemento"
+			unless other.instance_of?Matrices || alto!=other.ancho
+				Resultado[ancho*ancho]
+				for i in 0..ancho
+       				 for j in 0..ancho
+                			for k in 0..ancho
+                        		Resultado[i][j]+=matriz[i][k]*other.matriz[k][j]
+                			end		
+        			end
+				end				
+			end
+		#end
+	end
 	
+
 end
--- spec/Matrices_spec.rb
+++ spec/Matrices_spec.rb
@@ -2,6 +2,6 @@ require "Matrices.rb"
 
 describe Matrices do  
 	before :each do
-		@m1=Matrices.new()
+		#@m1=Matrices.new(Array=[1,2,3,4,5,6,7,8,9],3)
 	end
 end
